0aff267b4882feb11d912c16f88518b372ff4a18
Elgadisalma <elgadi.salma0@gmail.com>
2025-02-04 11:09:40 +0100

    RH-14 creer les composants necessaires

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'register', component: RegisterComponent }
+];
