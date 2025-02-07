6cdd1af4774eb32aa9c0de7f34a8cfdf5b21944c
Elgadisalma <elgadi.salma0@gmail.com>
2025-02-07 10:12:13 +0100

    RH-7 demander une collecte

---
+++ src/app/models/demande-collecte.model.spec.ts
@@ -0,0 +1,7 @@
+import { DemandeCollecte } from './demande-collecte.model';
+
+describe('DemandeCollecte', () => {
+  it('should create an instance', () => {
+    expect(new DemandeCollecte()).toBeTruthy();
+  });
+});
